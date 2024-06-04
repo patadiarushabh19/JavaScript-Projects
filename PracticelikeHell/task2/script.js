6e3fdcbd24bfbbc892e9b13cbaf93dc904c6656b
patadiarushabh19 <156648602+patadiarushabh19@users.noreply.github.com>
2024-06-04 13:51:33 +0530

    Update script.js

--- PracticelikeHell/task2/script.js
+++ PracticelikeHell/task2/script.js
@@ -1,1 +1,19 @@
+var img1 = document.querySelector("#one");
+var img2 = document.querySelector("#two");
+
+document.querySelector("button").addEventListener("click", function() {
+    img1.classList.add("animate");
+    img2.classList.add("animate");
+
+    setTimeout(function() {
+        var src1 = img1.src;
+        var src2 = img2.src;
+
+        img1.src = src2;
+        img2.src = src1;
+
+        img1.classList.remove("animate");
+        img2.classList.remove("animate");
+    }, 300);
+});
 
