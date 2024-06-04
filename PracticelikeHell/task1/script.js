f96767aefb8e55c7e917bb5c89e028be5cdba54f
patadiarushabh19 <156648602+patadiarushabh19@users.noreply.github.com>
2024-06-04 13:44:11 +0530

    Create script.js

---
+++ PracticelikeHell/task1/script.js
@@ -0,0 +1,13 @@
+var btn =  document.querySelector("button");
+ var p = document.querySelector("p");
+ 
+btn.addEventListener("click", function() {
+   p.textContent = "Hey Hello";
+   p.style.backgroundColor = "#d1d1d1";
+   p.style.textAlign = "center";
+   p.style.color = "crimson";
+   p.style.padding = "10px";
+   p.style.fontSize = "25px";
+   p.style.margin = "25px";
+   p.style.fontFamily = "gilroy";
+})
